7b44552cdd04fd4f6a4c7d01a4b1edd98b741b02
Jasminesonia <101569738+Jasminesonia@users.noreply.github.com>
2025-08-05 14:48:40 +0530

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://192.168.1.5:9007';
+const BASE_URL = 'https://phase1-fab7.onrender.com';
 
 export const api = axios.create({
   baseURL: BASE_URL,
